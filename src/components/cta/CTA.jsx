421be79458bc7bd49a3f13f2b6d56ef5e4ce5f0e
zubair480 <zubairzafar480@gmail.com>
2022-07-21 08:05:07 +0500

    Third commit

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './cta.css';
+import React from "react";
+import "./CTA.css";
 
 const CTA = () => (
   <div className="gpt3__cta">
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import './feature.css';
-
+import React from "react";
+import "./feature.css";
 const Feature = ({ title, text }) => (
   <div className="gpt3__features-container__feature">
     <div className="gpt3__features-container__feature-title">
